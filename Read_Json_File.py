a994c1ecba7b84fd9175650a2cf6334b56c9186c
rmit-s3511667-Ali-Alahmari <32062234+AliAla@users.noreply.github.com>
2019-03-20 12:12:31 +1100

    Reading Json file

---
+++ Read_Json_File.py
@@ -0,0 +1,34 @@
+import json
+from pathlib import Path
+
+# Reading Json file
+
+
+class ReadJsonFile():
+    def __init__(self):
+        self.__min_temperature, self.__max_temperature, self.__min_humidity, self.__max_humidity = self.__read()
+
+    @classmethod
+    def __read(self):
+        data = Path("config.json").read_text()
+        variables = json.loads(data)
+        __newlist = []
+        for key, value in variables.items():
+            __newlist.append(value)
+        return __newlist
+
+    @property
+    def min_temperature(self):
+        return self.__min_temperature
+
+    @property
+    def max_temperature(self):
+        return self.__max_temperature
+
+    @property
+    def min_humidity(self):
+        return self.__min_humidity
+
+    @property
+    def max_humidity(self):
+        return self.__max_humidity
