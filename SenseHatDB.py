d2c0b715e05110a81f81e59806bf115fb0fad2ea
unknown <s3593810@student.rmit.edu.au>
2019-03-25 19:14:19 +0600

    Insert Data in DataBase

--- SenseHatDB.py
+++ SenseHatDB.py
@@ -2,9 +2,16 @@ from datetime import datetime
 import sqlite3 as lite
 
 class SensehatDB:
-    
+
      def createDatabase(self):
         con = lite.connect('sensehat.db')
         with con: 
             cur = con.cursor() 
             cur.execute("CREATE TABLE IF NOT EXISTS SENSEHAT_data(timestamp DATETIME, temp NUMERIC, hum Numeric)")
+
+    def logData (self,temp,hum):	
+            conn=lite.connect('sensehat.db')
+            curs=conn.cursor()
+            curs.execute("INSERT INTO SENSEHAT_data values((?), (?), (?))", (datetime.now(), temp,hum,))
+            conn.commit()
+            conn.close()
