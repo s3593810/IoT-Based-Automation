a4ae8901bea44c21407baa1a5b51baa7bcc8a66b
unknown <s3593810@student.rmit.edu.au>
2019-03-25 19:12:09 +0600

    Create DataBase

--- SenseHatDB.py
+++ SenseHatDB.py
@@ -1,22 +1,10 @@
-import os, sys, time
 from datetime import datetime
-from sense_hat import SenseHat
 import sqlite3 as lite
 
-class SenseHatDB:
-    dbname='sensehat.db'
-    sampleFreq = 1 # time in seconds
+class SensehatDB:
     
-    def createDatabase():
+     def createDatabase(self):
         con = lite.connect('sensehat.db')
         with con: 
             cur = con.cursor() 
             cur.execute("CREATE TABLE IF NOT EXISTS SENSEHAT_data(timestamp DATETIME, temp NUMERIC, hum Numeric)")
-            
-    def displayData():
-        conn=lite.connect(dbname)
-        curs=conn.cursor()
-        print ("\nEntire database contents:\n")
-        for row in curs.execute("SELECT * FROM SenseHat_data"):
-            print (row)
-        conn.close()
