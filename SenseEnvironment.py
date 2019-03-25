5dec9d67dc188b2a1282e9151dacecbb82c2dffe
unknown <s3593810@student.rmit.edu.au>
2019-03-25 18:20:07 +0600

    Deducting CPU Temperature

--- SenseEnvironment.py
+++ SenseEnvironment.py
@@ -1,9 +1,16 @@
+import os, sys, time
 from sense_hat import SenseHat
 
 class SenseEnvironment:
+
+      def getCPUtemperature(self):
+        res = os.popen('vcgencmd measure_temp').readline()
+        return(float(res.replace("temp=","").replace("'C\n","")))
+	
 
       def sense(self):
         sense = SenseHat()
         temp = sense.get_temperature()
         hum = sense.get_humidity()
         hum = sense.get_humidity()
+        temp = temp - ((self.getCPUtemperature() - temp)/1)
