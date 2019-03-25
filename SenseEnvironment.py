c6948d946e681c2a6d36ea126a065b03800a8ff2
unknown <s3593810@student.rmit.edu.au>
2019-03-25 13:46:31 +0600

    Added method to sense

--- SenseEnvironment.py
+++ SenseEnvironment.py
@@ -1,3 +1,9 @@
+from sense_hat import SenseHat
 
 class SenseEnvironment:
 
+      def sense(self):
+        sense = SenseHat()
+        temp = sense.get_temperature()
+        hum = sense.get_humidity()
+        hum = sense.get_humidity()
