88d707c9908779b62527e2856e09e710b0aa7dce
unknown <s3593810@student.rmit.edu.au>
2019-03-28 10:05:31 +0600

    Updating Private Connection

--- SenseHatDB.py
+++ SenseHatDB.py
@@ -2,24 +2,21 @@ from datetime import datetime
 import sqlite3 as lite
 
 class SensehatDB:
-
-     def createDatabase(self):
-        connection = lite.connect('sensehat.db')
-        with connection: 
-            point = connection.cursor() 
+    __connection = lite.connect('sensehat.db')
+    def createDatabase(self):
+        with self.__connection: 
+            point = self.__connection.cursor() 
             point.execute("CREATE TABLE IF NOT EXISTS SENSEHAT_data(timestamp DATETIME, temp NUMERIC, hum Numeric)")
             
-        def InsertData (self,temp,hum):	
-            Lconn=lite.connect('sensehat.db')
-            point=connection.cursor()
+    def InsertData (self,temp,hum):	
+            point=self.__connection.cursor()
             point.execute("INSERT INTO SENSEHAT_data values((?), (?), (?))", (datetime.now(), temp,hum,))
-            connection.commit()
-            connection.close()
+            self.__connection.commit()
+            self.__connection.close()
 
-        def displayDB(self):
-            connection=lite.connect('sensehat.db')
-            point=connection.cursor()
+    def displayDB(self):
+            point=self.__connection.cursor()
             print ("\nEntire database contents:\n")
             for row in point.execute("SELECT * FROM SenseHat_data"):
                 print (row)
-            connection.close()
+            self.__connection.close()
