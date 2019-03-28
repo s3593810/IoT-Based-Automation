47805e3d8cef1639e53bd81cf455f334cca03eeb
unknown <s3593810@student.rmit.edu.au>
2019-03-28 10:19:19 +0600

    Separation of the Methods Sense

--- SenseEnvironment.py
+++ SenseEnvironment.py
@@ -2,18 +2,19 @@ import os, sys, time
 from sense_hat import SenseHat
 
 class SenseEnvironment:
-
+      __sense = SenseHat()
       def getCPUtemperature(self):
         res = os.popen('vcgencmd measure_temp').readline()
         return(float(res.replace("temp=","").replace("'C\n","")))
 	
-
-      def sense(self):
-        sense = SenseHat()
-        temp = sense.get_temperature()
-        hum = sense.get_humidity()
-        hum = sense.get_humidity()
+      def senseTmp(self):
+        
+        temp = self.__sense.get_temperature()
         temp = temp - ((self.getCPUtemperature() - temp)/1)
-        if temp is not None:
-            temp = round(temp, 1)
-            hum = round(hum,1)
+        temp = round(temp, 1)
+        return temp
+      
+      def senseHum(self):
+        hum = self.__sense.get_humidity()
+        hum = round(hum,1)
+        return hum
